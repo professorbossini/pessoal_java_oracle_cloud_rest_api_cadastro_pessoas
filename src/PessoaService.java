0c3a995feac35dd0ce09b5ea4b0db03202b19516
Rodrigo Bossini <professorbossini@gmail.com>
2022-10-27 15:06:22 -0300

    org.json extraindo propriedades e exibindo em loop

--- src/PessoaService.java
+++ src/PessoaService.java
@@ -3,6 +3,9 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse.BodyHandlers;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -18,7 +21,23 @@ public class PessoaService {
         .uri(URI.create(url))
         .build();
     var res = client.send(req, BodyHandlers.ofString());
-    System.out.println(res.body());
-  }
-  
+    // System.out.println(res.body());
+    //obtemos a raiz do resultado que é um objeto JSON
+    JSONObject raiz = new JSONObject(res.body());
+    // System.out.println(raiz);
+    // dela pegamos a propriedade items, que é um Array, uma coleção
+    JSONArray items = raiz.getJSONArray("items");
+    // System.out.println(items);
+    //iteramos sobre a coleção
+    //cada valor dentro dela é um objeto JSON
+    for (int i = 0; i < items.length(); i++){
+      JSONObject pessoa = items.getJSONObject(i);
+      // System.out.println(pessoa);
+      //de cada pessoa, pegamos nome, idade e hobby
+      System.out.println("Nome: " + pessoa.getString("nome"));
+      System.out.println("Idade: " + pessoa.getInt("idade"));
+      System.out.println("Hobby: " + pessoa.getString("hobby"));
+      System.out.println("**************************");
+    }
+  }  
 }
